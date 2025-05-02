c625c5b0d7541c896b1d565da8b8f9533094cc34
sports_app <rodinamohamad2003@gmail.com>
2025-05-03 02:26:18 +0300

    registerationSscreens+Mqtt+Doctorhome

---
+++ lib/core/widgets/AnimatedStatusIndicator.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class AnimatedStatusIndicator extends StatefulWidget {
+  final StatusItemData item;
+
+  const AnimatedStatusIndicator({Key? key, required this.item}) : super(key: key);
+
+  @override
+  _AnimatedStatusIndicatorState createState() => _AnimatedStatusIndicatorState();
+}
+
+class _AnimatedStatusIndicatorState extends State<AnimatedStatusIndicator> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _glowAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Duration duration = _getAnimationDuration(widget.item.label);
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: duration,
+    )..repeat(reverse: true);
+
+    _scaleAnimation = Tween<double>(begin: 0.95, end: 1.05).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    _glowAnimation = Tween<double>(begin: 5.0, end: 15.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  Duration _getAnimationDuration(String label) {
+    final lowerLabel = label.toLowerCase();
+    if (lowerLabel == 'very bad' ||
+        lowerLabel == 'bad' ||
+        lowerLabel == 'good' ||
+        lowerLabel == 'very good') {
+      return const Duration(milliseconds: 700);
+    }
+    return const Duration(milliseconds: 1500);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final circleSize = 100.0; // Match PatientHome's 100-pixel size
+    final innerCircleSize = circleSize * 0.7;
+
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _scaleAnimation.value,
+          child: Container(
+            width: circleSize,
+            height: circleSize,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: widget.item.gradient,
+              boxShadow: [
+                BoxShadow(
+                  color: widget.item.getGlowColor().withOpacity(0.5),
+                  blurRadius: _glowAnimation.value,
+                  spreadRadius: _glowAnimation.value / 2,
+                ),
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 6,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Container(
+                width: innerCircleSize,
+                height: innerCircleSize,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Text(
+                    widget.item.label,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: screenWidth < 600 ? 12.0 : 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF5DC1C3),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class StatusItemData {
+  final String label;
+  late final Gradient gradient;
+
+  StatusItemData({required this.label}) {
+    final lowerLabel = label.toLowerCase();
+    if (lowerLabel == 'good' || lowerLabel == 'very good') {
+      gradient = const LinearGradient(
+          colors: [Color(0xFF0EF816), Color(0xFFFBC204)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight);
+    } else if (lowerLabel == 'bad') {
+      gradient = const LinearGradient(
+          colors: [Color(0xFFFF0000), Color(0xFF0EF816), Color(0xFFFBC204)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight);
+    } else if (lowerLabel == 'very bad') {
+      gradient = const LinearGradient(
+          colors: [Color(0xFFFF0000), Color(0xFFFBC204)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight);
+    } else {
+      gradient = const LinearGradient(colors: [Colors.grey, Colors.grey]);
+    }
+  }
+
+  Color getGlowColor() {
+    final lowerLabel = label.toLowerCase();
+    if (lowerLabel == 'good' || lowerLabel == 'very good') {
+      return const Color(0xFF0EF816);
+    } else if (lowerLabel == 'bad' || lowerLabel == 'very bad') {
+      return const Color(0xFFFF0000);
+    } else {
+      return Colors.grey;
+    }
+  }
+}
+
+double calculateFontSize(double screenWidth) {
+  if (screenWidth >= 1200) {
+    return 18;
+  } else if (screenWidth >= 800) {
+    return 16;
+  } else if (screenWidth >= 400) {
+    return 14;
+  } else {
+    return 10;
+  }
+}
--- lib/pages/PatientHome.dart
+++ lib/pages/PatientHome.dart
@@ -1,7 +1,329 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:grad_project/core/constants/colours/colours.dart';
+// import 'package:grad_project/core/widgets/HomeBottomBar.dart';
+// import 'package:grad_project/core/widgets/expansionTile.dart';
+// import 'package:grad_project/core/widgets/statusCards.dart';
+// import 'package:grad_project/cubits/HeartRate_events.dart';
+// import 'package:grad_project/cubits/HeartRate_states.dart';
+// import 'package:grad_project/cubits/MQTT__Temp_events.dart';
+// import 'package:grad_project/cubits/MQTT__Temp_states.dart';
+// import 'package:grad_project/cubits/OxygenRate_events.dart';
+// import 'package:grad_project/cubits/OxygenRate_states.dart';
+//
+// class PatientHome extends StatelessWidget {
+//   static const String routeName = 'PatientHome';
+//   const PatientHome({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final height = MediaQuery.of(context).size.height;
+//     final width = MediaQuery.of(context).size.width;
+//     final icons = [
+//       Icons.message_rounded,
+//       Icons.menu_rounded,
+//       Icons.notifications_none_rounded,
+//     ];
+//     final routes = [
+//       '/messages',
+//       '/menu',
+//       '/notifications',
+//     ];
+//     bool hasNotification = true;
+//
+//     return Scaffold(
+//       body: Container(
+//         height: height,
+//         width: width,
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(
+//             colors: [
+//               gradient1,
+//               gradient2,
+//             ],
+//             begin: Alignment.topLeft,
+//             end: Alignment.bottomRight,
+//           ),
+//         ),
+//         child: Column(
+//           children: [
+//             SizedBox(height: height * 0.062),
+//             Padding(
+//               padding: EdgeInsets.all(width * 0.01),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.start,
+//                 children: [
+//                   Row(
+//                     children: [
+//                       Expanded(
+//                         child: Divider(
+//                           color: secondary,
+//                           thickness: 0.5,
+//                           indent: width * 0.3,
+//                           endIndent: width * 0.02,
+//                         ),
+//                       ),
+//                       Image.asset(
+//                         "assets/nabdLogo.png",
+//                         width: width * 0.2,
+//                         height: height * 0.07,
+//                       ),
+//                       Expanded(
+//                         child: Divider(
+//                           color: secondary,
+//                           thickness: 0.5,
+//                           indent: width * 0.02,
+//                           endIndent: width * 0.3,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   SizedBox(height: height * 0.001),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: [
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.end,
+//                         children: [
+//                           Text(
+//                             "Welcome",
+//                             style: GoogleFonts.nunito(
+//                               fontSize: 20,
+//                               height: 1.4,
+//                               fontWeight: FontWeight.w700,
+//                               color: secondary,
+//                             ),
+//                             textAlign: TextAlign.right,
+//                           ),
+//                           Text(
+//                             "Mohamed Ahmed ...",
+//                             style: GoogleFonts.nunito(
+//                               fontSize: 10,
+//                               height: 1.4,
+//                               fontWeight: FontWeight.w700,
+//                               color: secondary,
+//                             ),
+//                             textAlign: TextAlign.right,
+//                           ),
+//                         ],
+//                       ),
+//                       SizedBox(width: width * 0.02),
+//                       CircleAvatar(
+//                         radius: width * 0.07,
+//                         backgroundImage:
+//                         const AssetImage("assets/patientAvatar.png"),
+//                       ),
+//                       SizedBox(width: width * 0.1),
+//                       Row(
+//                         children: List.generate(
+//                           icons.length,
+//                               (index) => GestureDetector(
+//                             onTap: () =>
+//                                 Navigator.pushNamed(context, routes[index]),
+//                             child: Stack(
+//                               children: [
+//                                 Padding(
+//                                   padding: EdgeInsets.all(width * 0.02),
+//                                   child: Container(
+//                                     width: width * 0.09,
+//                                     height: width * 0.12,
+//                                     decoration: BoxDecoration(
+//                                       color: secondary,
+//                                       shape: BoxShape.circle,
+//                                     ),
+//                                     child: Icon(
+//                                       icons[index],
+//                                       size: width * 0.06,
+//                                       color: primary,
+//                                     ),
+//                                   ),
+//                                 ),
+//                                 if (index == 2 && hasNotification)
+//                                   Positioned(
+//                                     bottom: width * 0.02,
+//                                     right: width * 0.02,
+//                                     child: Container(
+//                                       width: width * 0.04,
+//                                       height: width * 0.04,
+//                                       decoration: BoxDecoration(
+//                                         color: notifications,
+//                                         shape: BoxShape.circle,
+//                                       ),
+//                                     ),
+//                                   ),
+//                               ],
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             SizedBox(height: height * 0.01),
+//             Padding(
+//               padding: const EdgeInsets.all(5),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Container(
+//                     width: width * 0.4,
+//                     height: width * 0.4,
+//                     decoration: BoxDecoration(
+//                       color: lighterColor,
+//                       borderRadius: BorderRadius.circular(15),
+//                     ),
+//                     child: Column(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         Text(
+//                           "Current Status",
+//                           style: TextStyle(
+//                             color: secondary,
+//                             fontSize: 15,
+//                           ),
+//                         ),
+//                         const SizedBox(height: 10),
+//                         Stack(
+//                           alignment: Alignment.center,
+//                           children: [
+//                             Container(
+//                               width: 100,
+//                               height: 100,
+//                               decoration: const BoxDecoration(
+//                                 shape: BoxShape.circle,
+//                                 gradient: LinearGradient(
+//                                   colors: [Colors.green, Colors.yellow],
+//                                   begin: Alignment.topLeft,
+//                                   end: Alignment.bottomRight,
+//                                 ),
+//                               ),
+//                             ),
+//                             Container(
+//                               width: 70,
+//                               height: 70,
+//                               decoration: BoxDecoration(
+//                                 color: secondary,
+//                                 shape: BoxShape.circle,
+//                               ),
+//                             ),
+//                             Text(
+//                               "Very\nGood",
+//                               style: GoogleFonts.nunito(
+//                                 fontSize: 16,
+//                                 fontWeight: FontWeight.bold,
+//                                 color: Colors.black,
+//                               ),
+//                               maxLines: 2,
+//                               softWrap: true,
+//                               textAlign: TextAlign.center,
+//                             ),
+//                           ],
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Expanded(
+//               child: SingleChildScrollView(
+//                 child: Column(
+//                   children: [
+//                     SizedBox(height: height * 0.01),
+//                     SingleChildScrollView(
+//                       scrollDirection: Axis.horizontal,
+//                       child: Padding(
+//                         padding: const EdgeInsets.all(8.0),
+//                         child: Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                           children: [
+//                             BlocBuilder<HeartRateCubit, HeartRateState>(
+//                               builder: (context, state) {
+//                                 return StatusCard(
+//                                   image: Image.asset("assets/heartRate.png"),
+//                                   title: "Heart Rate",
+//                                   value: _getHeartRateValue(state),
+//                                   unit: "bpm",
+//                                 );
+//                               },
+//                             ),
+//                             SizedBox(width: width * 0.01),
+//                             BlocBuilder<TemperatureCubit, TemperatureState>(
+//                               builder: (context, state) {
+//                                 return StatusCard(
+//                                   image: Image.asset("assets/temp.png"),
+//                                   title: "Temperature",
+//                                   value: _getDisplayValue(state),
+//                                   unit: "°C",
+//                                 );
+//                               },
+//                             ),
+//                             SizedBox(width: width * 0.01),
+//                             BlocBuilder<OxygenRateCubit, OxygenRateState>(
+//                               builder: (context, state) {
+//                                 return StatusCard(
+//                                   image: Image.asset("assets/pressure.png"),
+//                                   title: "Oxygen",
+//                                   value: _getOxygenValue(state),
+//                                   unit: "%",
+//                                 );
+//                               },
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(height: height * 0.03),
+//                     const CustomExpansionTile(
+//                       title: "My Diagnoses",
+//                       content:
+//                       "Neu Heart hear stress Palpitat\n.\"\" It is recommended to jyjtyjt  but jgk tyjtdfx perform an ECG and thyroid function tests to confirm the diagnosis and rule out organic causes.",
+//                     ),
+//                     SizedBox(height: height * 0.03),
+//                     const CustomExpansionTile(
+//                       title: "My Medicine",
+//                       content: "No this time.",
+//                     ),
+//                     SizedBox(height: height * 0.057),
+//                     BottomNavWidget(),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//   String _getDisplayValue(TemperatureState state) {
+//     if (!state.isConnected) return "Connecting";
+//     if (state.error != null) return "Error";
+//     if (state.temperature == null || state.temperature!.isEmpty) return "--";
+//     final temp = double.tryParse(state.temperature!) ?? 0;
+//     return temp.toStringAsFixed(1);
+//   }
+//   String _getHeartRateValue(HeartRateState state) {
+//     if (!state.isConnected) return "Connecting";
+//     if (state.error != null) return "Error";
+//     if (state.heartRate == null || state.heartRate!.isEmpty) return "--";
+//     return state.heartRate!;
+//   }
+//   String _getOxygenValue(OxygenRateState state) {
+//     if (!state.isConnected) return "Connecting";
+//     if (state.error != null) return "Error";
+//     if (state.oxygenRate == null) return "--";
+//     return state.oxygenRate!.toStringAsFixed(1);
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grad_project/core/constants/colours/colours.dart';
+import 'package:grad_project/core/widgets/AnimatedStatusIndicator.dart';
 import 'package:grad_project/core/widgets/HomeBottomBar.dart';
 import 'package:grad_project/core/widgets/expansionTile.dart';
 import 'package:grad_project/core/widgets/statusCards.dart';
@@ -186,42 +508,9 @@ class PatientHome extends StatelessWidget {
                             fontSize: 15,
                           ),
                         ),
-                        SizedBox(height: 10),
-                        Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Container(
-                              width: 100,
-                              height: 100,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                gradient: LinearGradient(
-                                  colors: [Colors.green, Colors.yellow],
-                                  begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 70,
-                              height: 70,
-                              decoration: BoxDecoration(
-                                color: secondary,
-                                shape: BoxShape.circle,
-                              ),
-                            ),
-                            Text(
-                              "Very\nGood",
-                              style: GoogleFonts.nunito(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              maxLines: 2,
-                              softWrap: true,
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
+                        const SizedBox(height: 10),
+                        AnimatedStatusIndicator(
+                          item: StatusItemData(label: "Very Good"),
                         ),
                       ],
                     ),
@@ -289,7 +578,7 @@ class PatientHome extends StatelessWidget {
                       content: "No this time.",
                     ),
                     SizedBox(height: height * 0.057),
-                    BottomNavWidget(),
+                    BottomNavWidget()
                   ],
                 ),
               ),
@@ -299,6 +588,7 @@ class PatientHome extends StatelessWidget {
       ),
     );
   }
+
   String _getDisplayValue(TemperatureState state) {
     if (!state.isConnected) return "Connecting";
     if (state.error != null) return "Error";
